ad7d2f087b3511b13c75e6cd329328cfbdce4d3b
wenmingxing1 <1793701192@qq.com>
2018-02-03 15:54:38 +0800

    内存池实现

--- MemoryPool/MemoryPool.h
+++ MemoryPool/MemoryPool.h
@@ -13,8 +13,8 @@ public:
 
     //定义rebind<U>::other接口
     template <typename U> struct rebind {
-    typedef MemoryPool<U> other;
-    }
+        typedef MemoryPool<U> other;
+    };
 
     //默认构造，初始化所有的槽指针
     //不抛出异常
@@ -26,23 +26,65 @@ public:
     }
 
     //析构函数，销毁一个现有内存池
-    ~MemoryPool() noexcept;
+    ~MemoryPool() noexcept {
+        //循环销毁内存池中分配的内存区块
+        slot_pointer_ curr = currentBlock_;
+        while (curr != nullptr) {
+            slot_pointer_ prev = curr->next;
+            operator delete(reinterpret_cast<void*>(curr));
+            curr = prev;
+        }
+    }
 
     //同一时间只能分配一个对象，n和hint会被忽略
-    pointer allocate(size_t n = 1, const T* hint = 0);
+    pointer allocate(size_t n = 1, const T* hint = 0) {
+        //如果有空闲的对象槽，则直接将空闲区域交付给申请内存程序
+        if (freeSlot_ != nullptr) {
+            pointer result = reinterpret_cast<pointer>(freeSlot_);
+            freeSlot_ = freeSlot_->next;
+            return result;
+        }
+
+        else {
+            //如果当前区块的对象槽不够用了，则分配一个新的内存区块
+            if (currentSlot_ >= lastSlot_) {
+                //分配一个新的内存区块，并指向一个内存区块
+                data_pointer_ newBlock = reinterpret_cast<data_pointer_>(operator new(BlockSize));
+                reinterpret_cast<slot_pointer_>(newBlock)->next = currentBlock_;
+                currentBlock_ = reinterpret_cast<slot_pointer_>(newBlock);
+                //填补整个区块来满足元素内存区域的对齐要求
+                data_pointer_ body = newBlock + sizeof(slot_pointer_);
+                uintptr_t result = reinterpret_cast<uintptr_t>(body);
+                size_t bodyPadding = (alignof(slot_type_)-result) % alignof(slot_type_);
+                currentSlot_ = reinterpret_cast<slot_pointer_>(body + bodyPadding);
+                lastSlot_ = reinterpret_cast<slot_pointer_>(newBlock + BlockSize - sizeof(slot_type_));
+            }
+            return reinterpret_cast<pointer>(currentSlot_++);
+        }
+    }
 
     //销毁指针p指向的内存区块
-    void deallocate(pointer p, size_t n = 1);
+    void deallocate(pointer p, size_t n = 1) {
+        if (p != nullptr) {
+            //reinterpret_cast是强制类型转换符
+            //要访问next必须强制将p转换成slot_pointer
+            //
+            reinterpret_cast<slot_pointer_>(p)->next = freeSlot_;
+            freeSlot_ = reinterpret_cast<slot_pointer_>(p);     //设定为freeSlot_
+        }
+    }
 
     //调用构造函数，这里使用了C++11的可变参数模板
     template <typename U, typename... Args>
-    void construct(U* p, Args&&... args);
+    void construct(U* p, Args&&... args) {
+        new (p) U (std::forward<Args>(args)...);    //  调用新建对象的构造函数
+    }
 
     //销毁内存池中的对象，即调用对象的析构函数
     template <typename U>
     void destroy(U* p)
     {
-        P->~U();
+        p->~U();
     }
 
 
@@ -74,7 +116,7 @@ private:
     slot_pointer_ freeSlot_;
 
     //检查定义的内存池大小是否过小;static_assert如果第一个表达式为false，则会输出指定错误信息，并编译失败
-    static_assert(BlockSize >= 2*sizeof(solt_type_), "BlockSize too small.");
+    static_assert(BlockSize >= 2*sizeof(slot_type_), "BlockSize too small.");
 };
 
 #endif // MEMORYPOOL_H_INCLUDED
